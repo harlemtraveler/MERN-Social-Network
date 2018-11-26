4fe06c0aee7e359beb63935c3158afbe5aa41f4d
Tim <tjk315@gmail.com>
2018-11-25 23:51:27 -0500

    Implement Registration && Add Error Reducer

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,9 +1,19 @@
-import { TEST_DISPATCH } from './types';
+import axios from 'axios';
+import { GET_ERRORS } from './types';
 
-// Register User
-export const registerUser = (userData) => {
-  return {
-    type: TEST_DISPATCH,
-    payload: userData
-  }
+// Register User - the "history" arguement is the "this.props.history" param passed from component
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post('/api/users/register', userData)
+    .then(res => history.push('/login'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      }));
 };
+/*
+  - When fteching from back-end, we need to wait for response from backend, then we can dispatch
+  - The "thunk" middleware will be useful for this (also add "dispatch" to function).
+  - When performing an asynchronous AJAX request, we can't simply return the error. We must call the "dispatch" function.
+*/
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
-import axios from 'axios';
+// "withRouter" will allows us to pass actions redirect ability "this.props.history"
+// MUST wrap component name in "withRouter" when exported
+// i.e. "export default withRouter(myComponent)"
+import { withRouter } from 'react-router-dom';
 import classnames from 'classnames';
 // In order to use Redux with a component, import "connect" from 'react-redux'.
 // Also, a contianer is a react component that'll work with Redux.
@@ -21,10 +24,19 @@ class Register extends Component {
 
   }
 
+  // Runs when component recieves new properties
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
+  // Runs when an element with the "onChange" property is modified
   onChange = (e) => {
     this.setState({ [e.target.name]: e.target.value })
   }
 
+  // Runs when an element with the "onSubmit" property is submitted.
   onSubmit = (e) => {
     e.preventDefault();
 
@@ -36,22 +48,17 @@ class Register extends Component {
     };
 
     // Any action we bring in, we call through "props", where it's stored.
-    this.props.registerUser(newUser);
-
-    // axios
-    //   .post('/api/users/register', newUser)
-    //   .then(res => console.log(res.data))
-    //   .catch(err => this.setState({ errors: err.response.data }));
+    // By passing "this.props.history" as second param, we can redirect from within...
+    // the "registerUser" action (we normally can't use this redirect method in actions).
+    this.props.registerUser(newUser, this.props.history);
 
   }
 
   render() {
     const { errors } = this.state;
-    const { user } = this.props.auth;
 
     return (
       <div className="register">
-        { user ? user.name : null }
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
@@ -136,22 +143,54 @@ class Register extends Component {
 
 }
 
-Register.PropTypes = {
+Register.propTypes = {
   registerUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 }
 
 // Stores "auth" state from root reducer in a variable so it can be access later, like "this.props.auth.user"
 const mapStateToProps = (state) => ({
   // state.auth comes from the root reducer (i.e. index.js within "reducers" dir)
-  auth: state.auth
+  auth: state.auth,
+  errors: state.errors
 });
+
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
 
 /*
   NOTICE:
   When using Redux with a component, you must use connect to export the component:
 
     export default connect(firstParam, ObjToMapActions)(myComponentName)
-*/
 
-export default connect(mapStateToProps, { registerUser })(Register);
+------------------------------------------------------------------------------------
+
+  Package: withRouter
+
+  Steps to using withRouter:
+  1. Import the package:
+
+    import { withRouter } from 'react-router-dom';
+
+  2. Wrap exported component in "withRouter"
+
+    export default withRouter(myComponentName);
+
+  3. When calling an action, pass "this.props.history" to give it redirect ability:
+
+    this.props.myAction(firstParam, this.props.history)
+
+  4. Now you can redirect from within the action file:
+
+    export const registerUser = (userData, history) => dispatch => {
+      axios
+        .post('/api/users/register', userData)
+        .then(res => history.push('/login'))
+        .catch(err =>
+          dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+          }));
+    };
+*/
