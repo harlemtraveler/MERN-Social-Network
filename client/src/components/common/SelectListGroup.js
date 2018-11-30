5c3a76ef362c1ad8fec5cd1cff729e11854c42a4
Tim <tjk315@gmail.com>
2018-11-30 17:31:48 -0500

    Create additional Form Field Components

--- client/src/components/common/SelectListGroup.js
+++ client/src/components/common/SelectListGroup.js
@@ -2,30 +2,32 @@ import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-const TextFieldGroup = ({
+const SelectListGroup = ({
   name,
-  placeholder,
   value,
-  label,
   error,
   info,
-  type,
   onChange,
-  disabled
+  options
 }) => {
+  const selectOptions = options.map(option => (
+    <option key={ option.label } value={ option.value }>
+      { option.label }
+    </option>
+  ));
+
   return (
     <div className="form-group">
-      <input
-        type={ type }
+      <select
         className={ classnames('form-control form-control-lg', {
           'is-invalid': error
         }) }
-        placeholder={ placeholder }
         name={ name }
         value={ value }
         onChange={ onChange }
-        disabled={ disabled }
-      />
+      >
+        { selectOptions }
+      </select>
       { info && <small className="form-text text-muted">{ info }</small> }
       { error && (
         <div className="invalid-feedback">
@@ -36,19 +38,13 @@ const TextFieldGroup = ({
   )
 };
 
-TextFieldGroup.propTypes = {
+SelectListGroup.propTypes = {
   name: PropTypes.string.isRequired,
-  placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
   error: PropTypes.string, // Is String the correct data type?
   info: PropTypes.string,
-  type: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  disabled: PropTypes.string
+  options: PropTypes.array.isRequired
 }
 
-TextFieldGroup.defaultProps = {
-  type: 'text'
-}
-
-export default TextFieldGroup;
+export default SelectListGroup;
