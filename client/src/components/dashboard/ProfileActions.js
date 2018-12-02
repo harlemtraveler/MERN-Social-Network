068077530f8552fbd901ec17af9540be55616545
Timothy <timothy.keaveny@troyweb.com>
2018-12-02 16:41:36 -0500

    Edit "ProfileActions" functional component

--- client/src/components/dashboard/ProfileActions.js
+++ client/src/components/dashboard/ProfileActions.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ProfileActions = ({}) => (
+const ProfileActions = () => (
   <div className="btn-group mb-4" role="group">
     <Link to="/edit-profile" className="btn btn-light">
       <i className="fas fa-user-circle text-info mr-1" /> Edit Profile
