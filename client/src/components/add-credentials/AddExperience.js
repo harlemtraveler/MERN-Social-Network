d23a7a993e52423fefe1d913bd9fa2fccef40533
Tim <tjk315@gmail.com>
2018-12-03 16:55:52 -0500

    Add form functionality to "AddExperience" component

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -4,6 +4,7 @@ import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { addExperience } from '../../actions/profileActions';
 
 class AddExperience extends Component {
   constructor(props) {
@@ -22,6 +23,12 @@ class AddExperience extends Component {
     }
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onCheck = (e) => {
     this.setState({
       disabled: !this.state.disabled,
@@ -36,18 +43,18 @@ class AddExperience extends Component {
   onSubmit = (e) => {
     e.preventDefault();
 
-    console.log('submit');
-    // const profileData = {
-    //   company: this.state.company,
-    //   title: this.state.title,
-    //   location: this.state.location,
-    //   from: this.state.from,
-    //   to: this.state.to,
-    //   current: this.state.current,
-    //   description: this.state.description,
-    //   errors: this.state.errors,
-    //   disabled: this.state.disabled,
-    // }
+    // console.log('submit');
+    const expData = {
+      company: this.state.company,
+      title: this.state.title,
+      location: this.state.location,
+      from: this.state.from,
+      to: this.state.to,
+      current: this.state.current,
+      description: this.state.description
+    };
+
+    this.props.addExperience(expData, this.props.history);
   }
 
   render() {
@@ -148,6 +155,7 @@ class AddExperience extends Component {
 }
 
 AddExperience.propTypes = {
+  addExperience: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired,
 }
@@ -157,4 +165,4 @@ const mapStateToProps = state => ({
   errors: state.errors
 });
 
-export default connect(mapStateToProps)(withRouter(AddExperience));
+export default connect(mapStateToProps, { addExperience })(withRouter(AddExperience));
