88a17a742bf0e649ac2fb01921e4d2422fc6bae2
Tim <tjk315@gmail.com>
2018-12-03 17:36:14 -0500

    Create "AddEducation" form componenet && Add functionality to "AddEducation" form component

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -47,7 +47,21 @@ export const addExperience = (expData, history) => dispatch => {
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
-      }))
+      })
+    );
+}
+
+// Add Education
+export const addEducation = (eduData, history) => dispatch => {
+  axios
+    .post('/api/profile/education', eduData)
+    .then(res => history.push('/dashboard'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
 }
 
 // Delete account & profile
