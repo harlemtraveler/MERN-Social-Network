a37c3b586ec133f99154d7618da172df773ee280
Tim <tjk315@gmail.com>
2018-12-03 00:53:39 -0500

    Create "Experience" form for user profile

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,7 @@ import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
 
 // CSS
 import './App.css';
@@ -76,6 +77,9 @@ class App extends Component {
               </Switch>
               <Switch>
                 <PrivateRoute exact path="/edit-profile" component={ EditProfile } />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/add-experience" component={ AddExperience } />
               </Switch>
             </div>
             <Footer />
