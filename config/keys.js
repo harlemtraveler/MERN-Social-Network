1383295dad58d8db01e6823eaac171c1358eeb11
Timothy <timothy.keaveny@troyweb.com>
2018-12-06 18:05:10 -0500

    Implement secure handling of keys in repository

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,5 @@
-module.exports = {
-  // mongoURI: 'mongodb://tim:password1@ds161446.mlab.com:61446/merndb-devopsec',
-  // mongoURI: 'mongodb://tim:password1@ds121262.mlab.com:21262/mern-book',
-  mongoURI: 'mongodb://tim:password1@localhost:27017/merndb',
-  secretOrKey: 'supersecret'
-};
-
-// Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjMDMwZjFkOWQ2OGUxYTFlYTQyMTEyMCIsIm5hbWUiOiJib2JieSIsImF2YXRhciI6Ii8vd3d3LmdyYXZhdGFyLmNvbS9hdmF0YXIvYWI5N2MxMmQyZGUzYmJmNDIzZTliMmEwNjRlZDk3MjI_cz0yMDAmcj1yJmQ9bW0iLCJpYXQiOjE1NDM3MDQ0NTUsImV4cCI6MTU0MzcwODA1NX0.NkNzIZYaYUOKKYA51y7BTp8IIQ_cvlTtNil7j8gM-Nc
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
